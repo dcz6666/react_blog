77b12754dc4935fdcbf5f01230e2a7b1200427c4
王文成 <dcz6666@126.com>
2022-09-05 22:01:25 +0800

    合并

--- components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import styles from './Footer.module.css'
-const Footer = ()=>(
-    <div className={styles.footerDiv}>
-        <div>系统由 React+Node+Ant Desgin驱动 </div>
-        <div>JSPang.com</div>
-    </div>
-)
-
-export default Footer
--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css'
-import 'antd/dist/antd.css'
-import '../styles/pages/comm.css'
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
