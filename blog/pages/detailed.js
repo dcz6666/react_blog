68953c4ad45bfa861b537255e1322a705b7bf556
王文成 <dcz6666@126.com>
2022-09-06 09:07:26 +0800

    详情页面

--- blog/pages/detailed.js
+++ blog/pages/detailed.js
@@ -14,41 +14,26 @@ import Footer from '../components/Footer/Footer.js'
 
 import { CalendarOutlined, FolderOutlined, FireOutlined, ConsoleSqlOutlined } from '@ant-design/icons';
 
+import marked from 'marked'
+import hljs from 'highlight.js'
+import 'highlight.js/styles/monokai-sublime'
+
 export default function Detailed() {
-    let markdown='# P01:课程介绍和环境搭建\n' +
-  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
-  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
-   '**这是加粗的文字**\n\n' +
-  '*这是倾斜的文字*`\n\n' +
-  '***这是斜体加粗的文字***\n\n' +
-  '~~这是加删除线的文字~~ \n\n'+
-  '\`console.log(111)\` \n\n'+
-  '# p02:来个Hello World 初始Vue3.0\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n'+
-  '***\n\n\n' +
-  '# p03:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p04:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '#5 p05:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p06:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p07:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '``` var a=11; ```'
+    const render = new marked.Renderer()
+    marked.setOptions({
+        renderer: renderer, //这个是必须填写的，你可以通过自定义的Renderer渲染出自定义的格式
+        gfm: true, //启动类似Github样式的Markdown,填写true或者false
+        pedantic: false,//只解析符合Markdown定义的，不修正Markdown的错误。填写true或者false
+        sanitize: false, // 原始输出，忽略HTML标签，这个作为一个开发人员，一定要写flase
+        tables: true, //支持Github形式的表格，必须打开gfm选项
+        breaks: false, //支持Github换行符，必须打开gfm选项，填写true或者false
+        smartLists: true, //优化列表输出，这个填写ture之后，你的样式会好看很多，所以建议设置成ture
+        smartypants: false,//高亮显示规则 ，这里我们将使用highlight.js来完成
+        highlight: function (code) {
+                return hljs.highlightAuto(code).value;
+        }
+      });
+      let html = marked(props.article_content) 
     return (
         <div>
             <Head>
